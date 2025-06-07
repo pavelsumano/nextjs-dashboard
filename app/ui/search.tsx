d24635eee3600c679b18f472e1ab5e8626b8a585
pavel sumano <sumanoop@gmail.com>
2025-06-07 11:49:59 -0600

    add debounce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,5 @@
 'use client';
- 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
 
@@ -8,7 +8,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
     if (term) {
@@ -17,7 +17,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
  
   return (
     <div className="relative flex flex-1 flex-shrink-0">
